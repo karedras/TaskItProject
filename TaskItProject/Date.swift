fc6fd1fc9c7ed7497f19f0d4aa52974f0fd34058
Adriel Carsete <adrielcarsete@mac1733.local>
2015-05-29 21:05:33 -0300

    First version - TASK IT APP

---
+++ TaskItProject/Date.swift
@@ -0,0 +1,9 @@
+//
+//  Date.swift
+//  TaskItProject
+//
+//  Created by Adriel Carsete on 5/28/15.
+//  Copyright (c) 2015 Adriel Carsete. All rights reserved.
+//
+
+import Foundation
---
+++ TaskItProject/TaskCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskCell.swift
+//  TaskItProject
+//
+//  Created by Adriel Carsete on 5/28/15.
+//  Copyright (c) 2015 Adriel Carsete. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
